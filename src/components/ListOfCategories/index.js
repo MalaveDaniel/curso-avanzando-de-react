127fa3845e2d08d86eb7a4365d28b055029b7b18
MalaveDaniel04 <dmalave04@hotmail.com>
2021-12-06 09:42:18 -0400

    useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowfixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server-malave2000-malavedaniel.vercel.app/categories').then(res => res.json()).then(response => {
       setCategories(response)
     })
   }, [])
-  return (
-    <List>
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowfixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
       categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
         }
     </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }

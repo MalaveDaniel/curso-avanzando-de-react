e219c2168e10685c1cdab3adafdad94f63c5365a
MalaveDaniel04 <dmalave04@hotmail.com>
2022-01-19 13:20:39 -0400

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
 import { Detail } from './pages/detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar'
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 export const App = () => {
   return (
     <div>
@@ -15,6 +21,20 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
 

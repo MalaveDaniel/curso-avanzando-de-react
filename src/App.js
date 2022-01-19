cce694a214012fd7b911315ac49047a71ac054b6
MalaveDaniel04 <dmalave04@hotmail.com>
2022-01-19 11:26:41 -0400

    Creando la página Detail

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardwithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
+import { Detail } from './pages/detail'
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardwithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:categoryId' />
-          </Router>
-      }
-
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </div>
 
   )
---
+++ src/pages/detail.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { PhotoCardwithQuery } from '../container/PhotoCardWithQuery'
+
+export const Detail = ({ detailId }) => (
+  <PhotoCardwithQuery id={detailId} />
+)

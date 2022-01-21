986b48b3e5a8e16be4cf2bed18c3aa90e2d9660b
MalaveDaniel04 <dmalave04@hotmail.com>
2022-01-21 10:46:37 -0400

    Mutaciones para registro

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
 import { Form, Input, Button, Title } from './style'
+
 export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
+
   return (
     <Fragment>
     <Title>{title}</Title>
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Input placeholder='Email' {...email} />
       <Input placeholder='Password' type='password' {...password} />
       <Button>{ title }</Button>
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,11 +1,25 @@
 import React, { Fragment } from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
+
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
       ({ activateAuth }) => {
-        return <Fragment> <UserForm title='Registrarse' onSubmit={activateAuth} />
+        return <Fragment> 
+          <RegisterMutation>
+            {
+              (register) => {
+                const onSubmit= ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  register({ variables }).then(activateAuth)
+                }
+                return <UserForm title='Registrarse' onSubmit={onSubmit} />
+              }
+            }
+          </RegisterMutation>
           <UserForm title='Iniciar sesion' onSubmit={activateAuth} />
         </Fragment>
       }
